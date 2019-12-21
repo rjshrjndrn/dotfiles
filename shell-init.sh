be1b6de7be8c2ddafd58427a3fca7521415d152f
rjshrjndrn <rjshrjndrn@gmail.com>
2019-12-21 06:13:30 +0530

    updating initscript

--- shell-init.sh
+++ shell-init.sh
@@ -1,5 +1,9 @@
 # http://bit.do/shell-init
 # ref: https://www.atlassian.com/git/tutorials/dotfiles
+echo "vim-plug for nvim"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "setting up dot files"
 git clone --bare git@github.com:rjshrjndrn/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
