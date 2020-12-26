ebfe6a44c13c8590a0c6e61a390835d766d53471
Rajesh Rajendran <rjshrjndrn@gmail.com>
2020-12-26 15:39:27 +0530

    updating configs

---
+++ scripts/config_backup/firefox-arkenfox-userjs-overrides.js
@@ -0,0 +1,27 @@
+// USER CONFIGS //
+
+user_pref("dom.allow_cut_copy", true); // 2400
+/* google meet */
+user_pref("media.getusermedia.screensharing.enabled", true); // 2022
+user_pref("media.peerconnection.enabled", true); // 2001
+user_pref("media.peerconnection.ice.no_host", false); // 2001
+user_pref("dom.webaudio.enabled", true); // 2510
+user_pref("media.autoplay.blocking_policy", 0); // 2031
+
+// URL search
+user_pref("keyword.enabled", true); //0801
+user_pref("browser.search.suggest.enabled", true); //0807
+user_pref("browser.urlbar.suggest.searches", true); //0807
+user_pref("privacy.clearOnShutdown.history", false); // 2803
+
+// SSO login
+// user_pref("dom.popup_allowed_events", "change click dblclick auxclick mouseup pointerup notificationclick reset submit touchend contextmenu"); //2212
+
+// Fonts 1400
+
+user_pref("gfx.font_rendering.opentype_svg.enabled", true);
+// user_pref("gfx.font_rendering.graphite.enabled", true);
+
+// 2010
+user_pref("webgl.disabled", false);
+user_pref("webgl.enable-webgl2", true);
