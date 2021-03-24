cb29d504a6d7ac7dadd64c8cfd748897766ce87d
Rajesh Rajendran <rjshrjndrn@gmail.com>
2021-03-24 09:01:45 +0530

    Multi media playback

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -226,22 +226,22 @@ awful.mouse.append_global_mousebindings({
 awful.keyboard.append_global_keybindings({
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
-              {description = "show main menu", group = "awesome"}),
+    -- awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
+    --           {description = "show main menu", group = "awesome"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
+    -- awful.key({ modkey }, "x",
+    --           function ()
+    --               awful.prompt.run {
+    --                 prompt       = "Run Lua code: ",
+    --                 textbox      = awful.screen.focused().mypromptbox.widget,
+    --                 exe_callback = awful.util.eval,
+    --                 history_path = awful.util.get_cache_dir() .. "/history_eval"
+    --               }
+    --           end,
+    --           {description = "lua execute prompt", group = "awesome"}),
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey }, "p", function() menubar.show() end,
@@ -286,6 +286,33 @@ awful.keyboard.append_global_keybindings({
       'XF86AudioMute',
       volume_widget.toggle,
       {description = 'toggle mute', group = 'hotkeys'}
+    ),
+    awful.key(
+      {},
+      'XF86AudioPrev',
+      -- volume_widget.play,
+      function ()
+          os.execute("playerctl previous")
+      end,
+      {description = 'skip to previous track', group = 'hotkeys'}
+    ),
+    awful.key(
+      {},
+      'XF86AudioNext',
+      -- volume_widget.play,
+      function ()
+          os.execute("playerctl next")
+      end,
+      {description = 'skip to next track', group = 'hotkeys'}
+    ),
+    awful.key(
+      {},
+      'XF86AudioPlay',
+      -- volume_widget.play,
+      function ()
+          os.execute("playerctl play-pause")
+      end,
+      {description = 'toggle play', group = 'hotkeys'}
     ),
 })
 
