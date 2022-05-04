eb86653b6679e9aca2f98fc716fae1ce8b10726f
rjshrjndrn <rjshrjndrn@gmail.com>
2022-05-04 20:52:04 +0200

    chore(zsh): adding zsh plugins
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- zsh/.antigen_plugins.sh
+++ zsh/.antigen_plugins.sh
@@ -10,6 +10,10 @@ antigen bundle command-not-found
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
+antigen bundle zsh-users/zsh-autosuggestions
+
+antigen bundle zsh-users/zsh-completions
+antigen bundle rupa/z
 
 # Load the theme.
 # antigen theme robbyrussell
