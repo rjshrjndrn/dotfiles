d1dbfb5365dd9a38511588074a62e057c42fdc48
rjshrjndrn <rjshrjndrn@gmail.com>
2022-05-05 10:25:21 +0200

    chore(zsh): bindings and z overriding
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

---
+++ zsh/.key_bindings.sh
@@ -0,0 +1,7 @@
+bindkey -v
+bindkey -M viins '^r' fzf-history-widget # r for reverse history search
+bindkey -M viins '^x^v' fzf-cd-widget # v for jump
+bindkey -M viins '^x^f' fzf-file-widget # f for file
+bindkey -M viins '^t' transpose-chars # t for transpose
+bindkey -M viins '\ec' capitalize-word # c for capitalizae
+
