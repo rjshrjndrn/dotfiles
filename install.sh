33819fc495b2dbcc55b359a3797f6faeadfe8051
rjshrjndrn <rjshrjndrn@gmail.com>
2022-11-23 22:48:23 +0100

    Cloning nvim repo
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- install.sh
+++ install.sh
@@ -40,5 +40,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# Cloning the nvim repo
+rm -rf ~/.config/nvim
+
+git clone https://github.com/rjshrjndrn/nvim-basic-ide.git ~/.config/nvim
+
 # install neovim plugins
-nvim --headless +PlugInstall +qall
+nvim
