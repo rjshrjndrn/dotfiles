a40c60128fb50de4a8caa9ae5cd1038cdcf1da0a
rjshrjndrn <rjshrjndrn@gmail.com>
2024-10-23 02:58:43 +0200

    nix: removed duplicates

--- nix/nixpkgs/default.nix
+++ nix/nixpkgs/default.nix
@@ -1,9 +1,9 @@
-# nix-env -f default.nix -iA myPackages --arg includeDev false
+# nix-env -f default.nix -iA myPackages --arg include '{ extra = true; }'
 
-{ pkgs ? import <nixpkgs> {}, includeDev ? false}:
+{ include ? { } }:
 
 let
-  # Import nixpkgs with the allowUnfree configuration and other settings
+  # Import nixpkgs with the necessary configurations
   pkgs = import <nixpkgs> {
     config = {
       allowUnfree = true;
@@ -16,15 +16,52 @@ let
     };
   };
 
-  basicPackages = import ./packages/basic.nix { inherit pkgs; };
-  devPackages = if includeDev then import ./packages/frontend.nix { inherit pkgs; } else [];
+  # Define an attribute set mapping group names to their package files
+  packageGroups = {
+    basic = {
+      file = ./packages/basic.nix;
+      alwaysInclude = true; # Basic packages are always included
+    };
+    frontend = {
+      file = ./packages/frontend.nix;
+      alwaysInclude = true;
+    };
+    # Add more package groups here
+    # For example:
+    # extra = {
+    #   file = ./packages/extra.nix;
+    #   alwaysInclude = false;
+    # };
+  };
+
+  # Function to determine if a package group should be included
+  includeGroup = name: group:
+    group.alwaysInclude || (include.${name} or false);
+
+  # Function to import packages from a package group
+  importPackages = name: group:
+    if includeGroup name group then
+      import group.file { inherit pkgs; }
+    else
+      [];
+
+  # Map over packageGroups to get the list of packages
+  packagesList = builtins.concatLists (
+    # Uses builtins.mapAttrs to apply importPackages to each package group
+    #
+    # `builtins.mapAttrs importPackages packageGroups` applies importPackages to each attribute in packageGroups.
+    # This means for each name = group pair, it calls importPackages name group
+    builtins.attrValues (builtins.mapAttrs importPackages packageGroups)
+  );
+
 in
 {
+  # Build the environment with the combined packages
   myPackages = pkgs.buildEnv {
     name = "my-packages";
-    paths = basicPackages ++ devPackages;
+    paths = packagesList;
     pathsToLink = [ "/share" "/bin" ];
     extraOutputsToInstall = [ "man" "doc" ];
   };
-
 }
+
