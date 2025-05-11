dc684fe62284e6f49a03572f8bcfbdefec64a6a8
rjshrjndrn <rjshrjndrn@gmail.com>
2025-05-11 02:51:41 +0200

    Adding hack for reset nix paths

---
+++ hack/system_path.sh
@@ -0,0 +1,3 @@
+unset PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/linguini/apps/bin:/home/linguini/.krew/bin:/home/linguini/.local/bin:/home/linguini/go/bin
+echo $PATH
